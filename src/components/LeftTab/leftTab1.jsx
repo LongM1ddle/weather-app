693d0322ea75ac84763c9c2f3965c5e27f21f510
LongMiddle <d.gilmanov05@gmail.com>
2024-12-07 06:23:58 +0600

    modified:   src/components/LeftTab/leftTab1.jsx

--- src/components/LeftTab/leftTab1.jsx
+++ src/components/LeftTab/leftTab1.jsx
@@ -4,6 +4,7 @@ import weatherIcon from "../../assets/weather-icon-small.svg";
 import navigationIcon from "../../assets/navigation-icon.svg";
 import locationIcon from "../../assets/location.svg";
 import settingsIcon from "../../assets/settings-icon.svg";
+import pfp from "../../assets/profile.gif"
 
 export const LeftTab1 = () => {
   return (
@@ -12,7 +13,7 @@ export const LeftTab1 = () => {
         <Link to="">
           <img
             title="Профиль"
-            src="https://cdn.discordapp.com/attachments/1159160399397126245/1309533891656286218/n2dlcr6ublzd1.gif?ex=6753ba39&is=675268b9&hm=632716ec4a73240fbea35a14648ecc216333048efd5b3d143a43a062054e4dcb&"
+            src={pfp}
           ></img>
         </Link>
       </div>
